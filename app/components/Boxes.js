187070d3d50a2d81b795189ac95df7892f1a22b4
Mobin Amiri <amirialimobin@gmail.com>
2026-06-09 14:26:20 +0200

    Swap columns - 2 boxes left, 3 boxes right

--- app/components/Boxes.js
+++ app/components/Boxes.js
@@ -2,13 +2,13 @@ export default function Boxes() {
   return (
     <div className="boxes-container">
       <div className="column column-left">
-        <div className="box">1</div>
-        <div className="box">2</div>
-        <div className="box">3</div>
+        <div className="box"></div>
+        <div className="box"></div>
       </div>
       <div className="column column-right">
-        <div className="box">4</div>
-        <div className="box">5</div>
+        <div className="box"></div>
+        <div className="box"></div>
+        <div className="box"></div>
       </div>
     </div>
   )
