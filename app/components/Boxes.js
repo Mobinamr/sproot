0ac6c61730403a410ae8412cd352dbf2d714e9ec
Mobin Amiri <amirialimobin@gmail.com>
2026-06-10 17:18:02 +0200

    Added point grid to the first box

--- app/components/Boxes.js
+++ app/components/Boxes.js
@@ -2,7 +2,17 @@ export default function Boxes() {
   return (
     <div className="boxes-container">
       <div className="column column-left">
-        <div className="box"></div>
+        <div className="box">
+          <div className="point-grid">
+            {[...Array(10)].map((_, row) => (
+              <div key={row} className="point-row">
+                {[...Array(10)].map((_, col) => (
+                  <div key={col} className="point"></div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
         <div className="box"></div>
       </div>
       <div className="column column-right">
